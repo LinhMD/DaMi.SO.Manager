66d25d6d57f03273edc70452c1618dcd6f7e3286
LinhMD <maidinhlinh967@gmail.com>
2024-04-09 16:58:37 +0700

    fix_tab

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -77,3 +77,11 @@ function calculateTotal() {
     $(`#ConvertDiscAmount`).val(toCurrency(totaldisc));
     $(`#ConvertTotalAmount`).val(toCurrency(totalConvert + totaltax - totaldisc));
 }
+var triggerTabList = [].slice.call(document.querySelectorAll('#pills-tab button'))
+triggerTabList.forEach(function (triggerEl) {
+    var tabTrigger = new bootstrap.Tab(triggerEl)
+    triggerEl.addEventListener('click', function (event) {
+        event.preventDefault()
+        tabTrigger.show()
+    })
+})
